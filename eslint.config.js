55e6fc0666f88631e842a9e4ddd96155ec9c80f1
Px Cheah <11415686+pxcheah@users.noreply.github.com>
2024-12-26 17:34:50 +0800

    chore: Update lint rules and add scripts that run in git hooks
    - Add ESLint import rules
    - Run lint and format on staged files
    - Ensure commit message is linted, following conventional-commit rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
 import js from "@eslint/js";
 import globals from "globals";
 import eslintConfigPrettier from "eslint-config-prettier";
+import importX from "eslint-plugin-import-x";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import storybook from "eslint-plugin-storybook";
-import tseslint from "typescript-eslint";
+import tseslint, { parser as tsParser } from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["node_modules", "dist", "storybook-static", ".storybook"] },
+  { ignores: ["node_modules", "dist", "storybook-static", ".storybook", "eslint.config.js"] },
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
@@ -16,16 +17,30 @@ export default tseslint.config(
       ...tseslint.configs.stylisticTypeChecked,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: "latest",
       globals: globals.browser,
       parserOptions: {
         project: ["./tsconfig.node.json", "./tsconfig.app.json"],
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
   },
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
     plugins: {
       react,
       "react-hooks": reactHooks,
@@ -39,11 +54,46 @@ export default tseslint.config(
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
+      "react/no-unknown-property": ["error", { ignore: ["cmdk-*"] }],
+    },
+  },
+  importX.flatConfigs.recommended,
+  importX.flatConfigs.typescript,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "import-x/newline-after-import": [
+        "error",
+        {
+          count: 1,
+          considerComments: true,
+        },
+      ],
+      "import-x/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          pathGroups: [{ pattern: "@/**", group: "internal" }],
+          distinctGroup: false,
+          pathGroupsExcludedImportTypes: [],
+          "newlines-between": "never",
+          alphabetize: {
+            order: "asc",
+            orderImportKind: "asc",
+            caseInsensitive: false,
+          },
+        },
+      ],
     },
   },
   storybook.configs["flat/csf-strict"],
   {
-    files: ["**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
+    files: ["**/*.stories.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
     rules: {
       "storybook/no-title-property-in-meta": "off",
     },
