655fa13f10bbdebed04f6ae836c7be16adc8fe39
Px Cheah <11415686+pxcheah@users.noreply.github.com>
2024-12-27 02:47:47 +0800

    build: Configure transpilation and dts generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,51 @@
 import path from "path";
+import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react-swc";
+import { globSync } from "glob";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import packageJson from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    copyPublicDir: false,
+    sourcemap: false,
+    lib: {
+      name: "ui",
+      entry: Object.fromEntries(
+        globSync(["./src/components/**/*.tsx", "./src/lib/**/*.ts"])
+          .filter((file) => !file.endsWith(".stories.tsx"))
+          .map((file) => [path.parse(file).base, fileURLToPath(new URL(file, import.meta.url))]),
+      ),
+      formats: ["es"],
+      fileName: (_, entryName) => `${path.parse(entryName).name}.js`,
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(packageJson.dependencies),
+        /@phosphor-icons\/react*/,
+        "react/jsx-runtime",
+        "react-dom/client",
+      ],
+      output: {
+        preserveModules: true,
+      },
+    },
+  },
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+      tsconfigPath: "./tsconfig.app.json",
+      exclude: ["./src/components/**/*.stories.tsx"],
+    }),
+  ],
 });
