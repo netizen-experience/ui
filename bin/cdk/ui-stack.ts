afe508b722053a2611cd116748bacc941104a1c3
Px Cheah <11415686+pxcheah@users.noreply.github.com>
2024-12-30 23:17:34 +0800

    build(infra): Migrate from OAI to OAC
    OAI is a legacy feature which doesn't support granular policy configs

--- bin/cdk/ui-stack.ts
+++ bin/cdk/ui-stack.ts
@@ -8,16 +8,11 @@ export class UiStack extends Stack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, "OriginAccessIdentity");
-    const bucket = new s3.Bucket(this, "Bucket", {
-      accessControl: s3.BucketAccessControl.PRIVATE,
-    });
-    bucket.grantRead(originAccessIdentity);
-
-    const origin = origins.S3BucketOrigin.withOriginAccessIdentity(bucket, { originAccessIdentity });
-    const distribution = new cloudfront.Distribution(this, "Distribution", {
+    // Resources for hosting the Storybook static site
+    const bucket = new s3.Bucket(this, "StorybookBucket");
+    const distribution = new cloudfront.Distribution(this, "StorybookCdn", {
       defaultBehavior: {
-        origin,
+        origin: origins.S3BucketOrigin.withOriginAccessControl(bucket),
         allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
         cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
         cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD,
@@ -34,6 +29,6 @@ export class UiStack extends Stack {
     });
 
     new CfnOutput(this, "BucketName", { value: bucket.bucketName });
-    new CfnOutput(this, "DistributionDomainName", { value: distribution.distributionDomainName });
+    new CfnOutput(this, "CdnDomainName", { value: distribution.distributionDomainName });
   }
 }
