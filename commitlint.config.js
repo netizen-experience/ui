97acd0dc9e54e5ac5721f434c638c89df27f6692
Px Cheah <11415686+pxcheah@users.noreply.github.com>
2025-01-02 15:43:27 +0800

    chore: Add `release` scope to commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [2, "always", ["lib", "infra"]],
+    "scope-enum": [2, "always", ["lib", "infra", "release"]],
     "subject-case": [2, "always", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
   },
 };
