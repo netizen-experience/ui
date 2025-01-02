047400acd19211ed34fba5737ca7b3eba1236d45
Px Cheah <11415686+pxcheah@users.noreply.github.com>
2025-01-02 10:45:09 +0800

    chore(lib): Remove unused vite plugins as build process has been migrated to rollup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,9 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
-  stories: ["../src/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-onboarding", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   core: {
     disableTelemetry: true,
@@ -11,6 +13,10 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ["../public"],
+  viteFinal: (config) =>
+    mergeConfig(config, {
+      plugins: [tsconfigPaths()],
+    }),
 };
 
 export default config;
