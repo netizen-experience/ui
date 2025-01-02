10ea22a35c957a8ae2f7f465c7746f02f0feebb5
Px Cheah <11415686+pxcheah@users.noreply.github.com>
2025-01-02 09:38:04 +0800

    chore: Untrack files inside dist directory

--- dist/focusable.d.ts
+++
@@ -1,5 +0,0 @@
-import { JSX } from 'react/jsx-runtime';
-
-export declare function Focusable({ className, ...props }: React.HTMLAttributes<HTMLElement>): JSX.Element;
-
-export { }
--- dist/textarea.d.ts
+++
@@ -1,8 +0,0 @@
-import { ForwardRefExoticComponent } from 'react';
-import { RefAttributes } from 'react';
-
-export declare const Textarea: ForwardRefExoticComponent<TextareaProps & RefAttributes<HTMLTextAreaElement>>;
-
-export declare type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
-
-export { }
